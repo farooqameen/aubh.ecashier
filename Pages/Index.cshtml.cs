118b827a510294c4e540a13a80450bd03869516b
farooqameen <102469559+farooqameen@users.noreply.github.com>
2024-08-19 23:41:18 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aubh.ecashier.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public Service? Service { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace aubh.ecashier
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        public ServiceEnum Options { get; set; }    
+    }
+
+    public enum ServiceEnum
+    {
+        [Description("Accomodation Fees")]
+        Accomodation,
+
+        [Description("Books")]
+        Books,
+
+        [Description("Late Payment Fee")]
+        LatePaymentFee,
+    }
+}
