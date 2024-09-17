110f96b48a420bcc9f92395603ad16e351db24cb
farooqameen <102469559+farooqameen@users.noreply.github.com>
2024-09-17 09:42:42 +0300

    Implemented intl-tel-input and reformatted faculty page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const input = document.querySelector("#phone");
+var iti = window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: callback => {
+        fetch("https://ipapi.co/json")
+            .then(res => res.json())
+            .then(data => callback(data.country_code))
+            .catch(() => callback("BH"))
+    },
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@24.5.0/build/js/utils.js",
+});
+window.iti = iti;
