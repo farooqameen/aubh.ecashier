1e0a8eeee99e113db10bc4d2441fbee7eb1f1a99
farooqameen <102469559+farooqameen@users.noreply.github.com>
2024-11-13 00:23:47 +0300

    Added responsiveness to sidebar

---
+++ wwwroot/js/sidebar.js
@@ -0,0 +1,16 @@
+﻿const currentPath = window.location.pathname.toLowerCase();
+const navItems = document.getElementsByName("nav");
+
+navItems.forEach(link => {
+    const linkPath = link.getAttribute('href')?.toLowerCase() || '';
+    if (
+        currentPath === linkPath ||
+        currentPath.startsWith(linkPath + '/') ||
+        (linkPath === '/' && currentPath === '/')
+    ) {
+        link.classList.add('btn-primary');
+        
+    } else {
+        link.classList.remove('btn-primary');
+    }
+});
